03ff9bbfdc16c7f6faaf4fd06ab58017a304e6ea
Roshan Kumar <roshankumarrk9570@gmail.com>
2025-07-02 19:34:07 +0530

    add tours

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-    return(
-      <main id="main">
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setTours(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const removeTour = (id) => {
+    setTours(tours.filter((tour) => tour.id !== id));
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  if (tours.length === 0)
+    return (
+      <main>
+        <h2>No tours left</h2>
+        <button className="btn" onClick={fetchTours}>Refresh</button>
       </main>
-    )
-}
+    );
+
+  return (
+    <main>
+      <h2>Our Tours</h2>
+      <div className="underline"></div>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+};
+
 export default App;
---
+++ src/components/Tour.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, name, image, info, price, removeTour }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {showMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setShowMore(!showMore)}>
+            {showMore ? "See less" : "Show more"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Remove
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
